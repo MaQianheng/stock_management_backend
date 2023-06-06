46205e23465ec2faa46f0c24c4ea47110a3fcc72
青尚 <maqianheng.mqh@alibaba-inc.com>
2023-06-06 14:34:47 +0800

    refactor

--- functions/db_func.js
+++ functions/db_func.js
@@ -1,208 +1,256 @@
-const {funcCurrentPage} = require("./utils");
-const {filterObjValue} = require("./validate");
-const {objectToString} = require("./utils")
+const { funcCurrentPage } = require('./utils');
+const { filterObjValue } = require('./validate');
+const { objectToString } = require('./utils');
 
 // todo: remove this func
 exports.dbCurrentPage = (dataSource) => {
-    const {currentPageCount} = dataSource
-    let intCurrentPageCount
-    try {
-        if (!currentPageCount) {
-            intCurrentPageCount = 1
-        } else {
-            intCurrentPageCount = parseInt(currentPageCount)
-        }
-    } catch (err) {
-        intCurrentPageCount = 1
+  const { currentPageCount } = dataSource;
+  let intCurrentPageCount;
+  try {
+    if (!currentPageCount) {
+      intCurrentPageCount = 1;
+    } else {
+      intCurrentPageCount = parseInt(currentPageCount);
     }
-    return intCurrentPageCount
-}
+  } catch (err) {
+    intCurrentPageCount = 1;
+  }
+  return intCurrentPageCount;
+};
 
 exports.dbAdd = (req, res, model, objAddData) => {
-    objAddData = filterObjValue(objAddData)
-    model(objAddData).save((err, data) => {
-        if (err) {
-            return res.status(500).json({
-                err_code: 1,
-                message: `提交失败：${err.message}`
-            })
-        }
-        return res.status(200).json({
-            err_code: 0,
-            message: "提交成功",
-            data
-        })
-    })
-}
+  objAddData = filterObjValue(objAddData);
+  model(objAddData).save((err, data) => {
+    if (err) {
+      return res.status(500).json({
+        err_code: 1,
+        message: `提交失败：${err.message}`,
+      });
+    }
+    return res.status(200).json({
+      err_code: 0,
+      message: '提交成功',
+      data,
+    });
+  });
+};
 
 exports.dbAddSync = (model, objAddData) => {
-    objAddData = filterObjValue(objAddData)
-    return model(objAddData).save();
-}
+  objAddData = filterObjValue(objAddData);
+  return model(objAddData).save();
+};
 
 exports.dbIfDataUniqueSync = async (model, objFilter) => {
-    objFilter = filterObjValue(objFilter)
-    objFilter.isDeleted = false
-    return await model.findOne(objFilter).exec()
-}
+  objFilter = filterObjValue(objFilter);
+  objFilter.isDeleted = false;
+  return await model.findOne(objFilter).exec();
+};
 
 exports.dbAddUnique = async (req, res, model, objFilter, objAddData) => {
-    let data
-    try {
-        data = await this.dbIfDataUniqueSync(model, objFilter)
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `提交失败：${err.message}`
-        })
-    }
-    if (data) {
-        return res.status(500).json({
-            err_code: 2,
-            message: `${objectToString(objFilter)}已存在`
-        })
-    }
-    this.dbAdd(req, res, model, objAddData)
-}
+  let data;
+  try {
+    data = await this.dbIfDataUniqueSync(model, objFilter);
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `提交失败：${err.message}`,
+    });
+  }
+  if (data) {
+    return res.status(500).json({
+      err_code: 2,
+      message: `${objectToString(objFilter)}已存在`,
+    });
+  }
+  this.dbAdd(req, res, model, objAddData);
+};
 
 exports.dbAddUniqueSync = async (model, objFilter, objAddData) => {
-    let data = await this.dbIfDataUniqueSync(model, objFilter)
-    if (data) throw {message: `数据已存在：${data}`}
-    objAddData = filterObjValue(objAddData)
-    return await this.dbAddSync(model, objAddData)
-}
+  let data = await this.dbIfDataUniqueSync(model, objFilter);
+  if (data) throw { message: `数据已存在：${data}` };
+  objAddData = filterObjValue(objAddData);
+  return await this.dbAddSync(model, objAddData);
+};
 
-exports.dbQueryList = (req, res, model, perPageCount = 0, objFilter = {}, populateArr = "") => {
-    objFilter = filterObjValue(objFilter)
-    let intCurrentPageCount = funcCurrentPage(req)
+exports.dbQueryList = (
+  req,
+  res,
+  model,
+  perPageCount = 0,
+  objFilter = {},
+  populateArr = ''
+) => {
+  objFilter = filterObjValue(objFilter);
+  let intCurrentPageCount = funcCurrentPage(req);
 
-    // objFilter.isDeleted = false
-    model.countDocuments(objFilter, function (err, dataCount) {
+  // objFilter.isDeleted = false
+  model.countDocuments(objFilter, function (err, dataCount) {
+    if (err) {
+      return res.status(500).json({
+        err_code: 1,
+        message: `读取数据失败：${err.message}`,
+      });
+    }
+    model
+      .find(objFilter, { __v: 0 }, (err, data) => {
         if (err) {
-            return res.status(500).json({
-                err_code: 1,
-                message: `读取数据失败：${err.message}`
-            })
+          return res.status(500).json({
+            err_code: 1,
+            message: `读取数据失败：${err.message}`,
+          });
         }
-        model.find(objFilter, {__v: 0}, (err, data) => {
-            if (err) {
-                return res.status(500).json({
-                    err_code: 1,
-                    message: `读取数据失败：${err.message}`
-                })
-            }
-            for (let i = 0; i < data.length; i++) {
-                // 0: no action, 1: edited, 2: uploading
-                data[i]["_doc"]["status"] = 0
-                data[i]["_doc"]["row"] = i
-            }
-            // level: {value: "0", text: '根级管理员'}
-            return res.status(200).json({
-                err_code: 0,
-                message: "请求成功",
-                dataCount,
-                data
-            })
-        }).populate(populateArr).skip(((intCurrentPageCount === 0 ? 1 : intCurrentPageCount) - 1) * perPageCount).limit(intCurrentPageCount === 0 ? 0 : perPageCount);
-    });
-}
+        for (let i = 0; i < data.length; i++) {
+          // 0: no action, 1: edited, 2: uploading
+          data[i]['_doc']['status'] = 0;
+          data[i]['_doc']['row'] = i;
+        }
+        // level: {value: "0", text: '根级管理员'}
+        return res.status(200).json({
+          err_code: 0,
+          message: '请求成功',
+          dataCount,
+          data,
+        });
+      })
+      .populate(populateArr)
+      .skip(
+        ((intCurrentPageCount === 0 ? 1 : intCurrentPageCount) - 1) *
+          perPageCount
+      )
+      .limit(intCurrentPageCount === 0 ? 0 : perPageCount);
+  });
+};
 
 exports.dbQueryOptions = (req, res, model, objFilter, strTextField) => {
-    objFilter = filterObjValue(objFilter)
-    // 如果id是0，则代表请求全部
-    if (!objFilter._id) delete objFilter._id
-    objFilter.isDeleted = false
-    model.find(objFilter, {__v: 0}, (err, data) => {
-        if (err) {
-            return res.status(500).json({
-                err_code: 1,
-                message: `读取数据失败：${err.message}`
-            })
-        }
-        // exReturn: [{value: 1, text: '金色'}, {value: 2, text: '红色'}]
-        if (data) {
-            for (let i = 0; i < data.length; i++) {
-                const value = data[i]["_id"]
-                const text = data[i][strTextField]
-                data[i] = {value, text}
-            }
-        }
-        return res.status(200).json({
-            err_code: 0,
-            message: "请求成功",
-            data
-        })
-    })
-}
+  objFilter = filterObjValue(objFilter);
+  // 如果id是0，则代表请求全部
+  if (!objFilter._id) delete objFilter._id;
+  objFilter.isDeleted = false;
+  model.find(objFilter, { __v: 0 }, (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        err_code: 1,
+        message: `读取数据失败：${err.message}`,
+      });
+    }
+    // exReturn: [{value: 1, text: '金色'}, {value: 2, text: '红色'}]
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        const value = data[i]['_id'];
+        const text = data[i][strTextField];
+        data[i] = { value, text };
+      }
+    }
+    return res.status(200).json({
+      err_code: 0,
+      message: '请求成功',
+      data,
+    });
+  });
+};
 
 exports.dbQueryListCountSync = async (model, objFilter) => {
-    objFilter = filterObjValue(objFilter)
-    return await model.countDocuments(objFilter).exec()
-}
+  objFilter = filterObjValue(objFilter);
+  return await model.countDocuments(objFilter).exec();
+};
 
-exports.dbQueryListSync = async (req, res, model, perPageCount = 0, objFilter = {}, stringRequiredFields = "", populateArr = "") => {
-    objFilter = filterObjValue(objFilter)
-    let intCurrentPageCount = funcCurrentPage(req)
-    return await model.find(objFilter, stringRequiredFields).populate(populateArr).skip(((intCurrentPageCount === 0 ? 1 : intCurrentPageCount) - 1) * perPageCount).limit(intCurrentPageCount === 0 ? 0 : perPageCount).exec()
-}
+exports.dbQueryListSync = async (
+  req,
+  res,
+  model,
+  perPageCount = 0,
+  objFilter = {},
+  stringRequiredFields = '',
+  populateArr = ''
+) => {
+  objFilter = filterObjValue(objFilter);
+  let intCurrentPageCount = funcCurrentPage(req);
+  return await model
+    .find(objFilter, stringRequiredFields)
+    .populate(populateArr)
+    .skip(
+      ((intCurrentPageCount === 0 ? 1 : intCurrentPageCount) - 1) * perPageCount
+    )
+    .limit(intCurrentPageCount === 0 ? 0 : perPageCount)
+    .exec();
+};
 
 exports.dbQuerySingleSync = async (model, objFilter) => {
-    objFilter = filterObjValue(objFilter)
-    return await model.findOne(objFilter).exec()
-}
+  objFilter = filterObjValue(objFilter);
+  return await model.findOne(objFilter).exec();
+};
 
 exports.dbUpdateById = (req, res, model, objFilter, objUpdateData) => {
-    objFilter = filterObjValue(objFilter)
-    objUpdateData = filterObjValue(objUpdateData)
-    model.findByIdAndUpdate(objFilter, objUpdateData, {}, (err, data) => {
-        if (err) {
-            return res.status(500).json({
-                err_code: 1,
-                message: `更新失败：${err.message}`
-            })
-        }
-        return res.status(200).json({
-            err_code: 0,
-            message: "更新成功"
-        })
+  objFilter = filterObjValue(objFilter);
+  objUpdateData = filterObjValue(objUpdateData);
+  model.findByIdAndUpdate(objFilter, objUpdateData, {}, (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        err_code: 1,
+        message: `更新失败：${err.message}`,
+      });
+    }
+    return res.status(200).json({
+      err_code: 0,
+      message: '更新成功',
     });
-}
+  });
+};
 
-exports.dbUpdateUniqueById = (req, res, model, _id, objFilter, objUpdateData) => {
-    objFilter = filterObjValue(objFilter)
-    objFilter.isDeleted = false
-    // check if there is any other data except this data that with ${_id}
-    model.findOne({_id: {$ne: _id}, ...objFilter}, (err, data) => {
-        if (err) {
-            return res.status(500).json({
-                err_code: 1,
-                message: `提交失败：${err.message}`
-            })
-        }
-        if (data) {
-            return res.status(200).json({
-                err_code: 2,
-                message: `${objectToString(objFilter)}已存在`
-            })
-        }
-        this.dbUpdateById(req, res, model, {_id: _id}, objUpdateData)
-    })
-}
+exports.dbUpdateUniqueById = (
+  req,
+  res,
+  model,
+  _id,
+  objFilter,
+  objUpdateData
+) => {
+  objFilter = filterObjValue(objFilter);
+  objFilter.isDeleted = false;
+  // check if there is any other data except this data that with ${_id}
+  model.findOne({ _id: { $ne: _id }, ...objFilter }, (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        err_code: 1,
+        message: `提交失败：${err.message}`,
+      });
+    }
+    if (data) {
+      return res.status(200).json({
+        err_code: 2,
+        message: `${objectToString(objFilter)}已存在`,
+      });
+    }
+    this.dbUpdateById(req, res, model, { _id: _id }, objUpdateData);
+  });
+};
 
 // {$set: {isDeleted: true}}
-exports.dbUpdateManyById = async (req, res, model, objParameters, objUpdateData) => {
-    const session = await model.startSession()
-    await session.withTransaction(async () => {
-        await model.updateMany({_id: {$in: objParameters._id}}, objUpdateData, {session})
-        return res.status(200).json({
-            err_code: 0,
-            message: `操作成功`
-        })
-    }).catch((err) => {
-        console.log(err.message)
-        return res.status(500).json({
-            err_code: 1,
-            message: `操作失败`
-        })
+exports.dbUpdateManyById = async (
+  req,
+  res,
+  model,
+  objParameters,
+  objUpdateData
+) => {
+  const session = await model.startSession();
+  await session
+    .withTransaction(async () => {
+      await model.updateMany(
+        { _id: { $in: objParameters._id } },
+        objUpdateData,
+        { session }
+      );
+      return res.status(200).json({
+        err_code: 0,
+        message: `操作成功`,
+      });
     })
-}
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({
+        err_code: 1,
+        message: `操作失败`,
+      });
+    });
+};
--- routes/color.js
+++ routes/color.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const {ColorModel} = require("../db/db_models")
-const {validateRequiredQueryParameters, authenticateJWT} = require('../functions/validate')
-const {dbQueryList, dbAddUnique, dbUpdateUniqueById, dbQueryOptions, dbUpdateManyById} = require('../functions/db_func')
+const express = require('express');
+const router = express.Router();
+const { ColorModel } = require('../db/db_models');
+const {
+  validateRequiredQueryParameters,
+  authenticateJWT,
+} = require('../functions/validate');
+const {
+  dbQueryList,
+  dbAddUnique,
+  dbUpdateUniqueById,
+  dbQueryOptions,
+  dbUpdateManyById,
+} = require('../functions/db_func');
 
 /**
  * err_code:
@@ -12,156 +21,175 @@ const {dbQueryList, dbAddUnique, dbUpdateUniqueById, dbQueryOptions, dbUpdateMan
  */
 
 router.get('/query', authenticateJWT, (req, res) => {
-    let objFilter = {}
-    try {
-        objFilter = validateRequiredQueryParameters(req, res, {
-            _id: {
-                type: 'String',
-                isRequired: false,
-                str: '颜色id'
-            }
-        })
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `${err}`
-        })
-    }
-    dbQueryList(req, res, ColorModel, 10, objFilter)
-})
+  let objFilter = {};
+  try {
+    objFilter = validateRequiredQueryParameters(req, res, {
+      _id: {
+        type: 'String',
+        isRequired: false,
+        str: '颜色id',
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `${err}`,
+    });
+  }
+  dbQueryList(req, res, ColorModel, 10, objFilter);
+});
 
 router.get('/query_color_options', authenticateJWT, (req, res) => {
-    try {
-        validateRequiredQueryParameters(req, res, {})
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `${err}`
-        })
-    }
-    dbQueryOptions(req, res, ColorModel, {}, "color")
-})
+  try {
+    validateRequiredQueryParameters(req, res, {});
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `${err}`,
+    });
+  }
+  dbQueryOptions(req, res, ColorModel, {}, 'color');
+});
 
 router.get('/add', authenticateJWT, async (req, res) => {
-    let objFilter = {}
-    try {
-        objFilter = validateRequiredQueryParameters(req, res, {
-            color: {
-                type: 'String',
-                isRequired: true,
-                str: '颜色'
-            }
-        })
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `${err}`
-        })
-    }
-    await dbAddUnique(req, res, ColorModel, {'color': objFilter.color}, {color: objFilter.color})
-})
+  let objFilter = {};
+  try {
+    objFilter = validateRequiredQueryParameters(req, res, {
+      color: {
+        type: 'String',
+        isRequired: true,
+        str: '颜色',
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `${err}`,
+    });
+  }
+  await dbAddUnique(
+    req,
+    res,
+    ColorModel,
+    { color: objFilter.color },
+    { color: objFilter.color }
+  );
+});
 
 router.get('/update', authenticateJWT, (req, res) => {
-    let objFilter = {}
-    try {
-        objFilter = validateRequiredQueryParameters(req, res, {
-            _id: {
-                type: 'String',
-                isRequired: true,
-                str: '颜色id'
-            },
-            color: {
-                type: 'String',
-                isRequired: true,
-                str: '颜色'
-            }
-        })
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `${err}`
-        })
-    }
-    dbUpdateUniqueById(req, res, ColorModel, objFilter._id, {'color': objFilter.color}, {color: objFilter.color})
-})
+  let objFilter = {};
+  try {
+    objFilter = validateRequiredQueryParameters(req, res, {
+      _id: {
+        type: 'String',
+        isRequired: true,
+        str: '颜色id',
+      },
+      color: {
+        type: 'String',
+        isRequired: true,
+        str: '颜色',
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `${err}`,
+    });
+  }
+  dbUpdateUniqueById(
+    req,
+    res,
+    ColorModel,
+    objFilter._id,
+    { color: objFilter.color },
+    { color: objFilter.color }
+  );
+});
 
 router.get('/delete', authenticateJWT, async (req, res) => {
-    let objParameters = {}
-    try {
-        objParameters = validateRequiredQueryParameters(req, res, {
-            _id: {
-                type: 'StringArray',
-                isRequired: true,
-                str: '颜色id'
-            }
-        })
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `${err}`
-        })
-    }
-    // const session = await ColorModel.startSession()
-    // await session.withTransaction(async () => {
-    //     // update productSub color to undefined
-    //     // await ProductModel.updateMany({colorRef: {$in: objParameters._id}}, {$set: {colorRef: undefinedColorId}}, {session})
-    //     // delete
-    //     const deleteRes = await ColorModel.updateMany({_id: {$in: objParameters._id}}, {$set: {isDeleted: true}}, {session})
-    //     return res.status(200).json({
-    //         err_code: 0,
-    //         message: `成功删除${deleteRes.deletedCount}条数据。`
-    //     })
-    // }).catch((err) => {
-    //     return res.status(500).json({
-    //         err_code: 1,
-    //         message: `删除失败：${err.message}`
-    //     })
-    // })
-    await dbUpdateManyById(req, res, ColorModel, objParameters, {$set: {isDeleted: true}})
-})
+  let objParameters = {};
+  try {
+    objParameters = validateRequiredQueryParameters(req, res, {
+      _id: {
+        type: 'StringArray',
+        isRequired: true,
+        str: '颜色id',
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `${err}`,
+    });
+  }
+  // const session = await ColorModel.startSession()
+  // await session.withTransaction(async () => {
+  //     // update productSub color to undefined
+  //     // await ProductModel.updateMany({colorRef: {$in: objParameters._id}}, {$set: {colorRef: undefinedColorId}}, {session})
+  //     // delete
+  //     const deleteRes = await ColorModel.updateMany({_id: {$in: objParameters._id}}, {$set: {isDeleted: true}}, {session})
+  //     return res.status(200).json({
+  //         err_code: 0,
+  //         message: `成功删除${deleteRes.deletedCount}条数据。`
+  //     })
+  // }).catch((err) => {
+  //     return res.status(500).json({
+  //         err_code: 1,
+  //         message: `删除失败：${err.message}`
+  //     })
+  // })
+  await dbUpdateManyById(req, res, ColorModel, objParameters, {
+    $set: { isDeleted: true },
+  });
+});
 
 router.get('/restore', authenticateJWT, async (req, res) => {
-    let objParameters = {}
-    try {
-        objParameters = validateRequiredQueryParameters(req, res, {
-            _id: {
-                type: 'StringArray',
-                isRequired: true,
-                str: '颜色id'
-            }
-        })
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `${err}`
-        })
-    }
-    await dbUpdateManyById(req, res, ColorModel, objParameters, {$set: {isDeleted: false}})
-})
+  let objParameters = {};
+  try {
+    objParameters = validateRequiredQueryParameters(req, res, {
+      _id: {
+        type: 'StringArray',
+        isRequired: true,
+        str: '颜色id',
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `${err}`,
+    });
+  }
+  await dbUpdateManyById(req, res, ColorModel, objParameters, {
+    $set: { isDeleted: false },
+  });
+});
 
 router.get('/update_delete_marker', authenticateJWT, async (req, res) => {
-    let objParameters = {}
-    try {
-        objParameters = validateRequiredQueryParameters(req, res, {
-            _id: {
-                type: 'StringArray',
-                isRequired: true,
-                str: '颜色id'
-            },
-            // 0: false, 1: true
-            action: {
-                type: 'Number',
-                isRequired: true,
-                str: '操作'
-            }
-        })
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 1,
-            message: `${err}`
-        })
-    }
-    await dbUpdateManyById(req, res, ColorModel, objParameters, {$set: {isDeleted: objParameters.action !== 1}})
-})
+  let objParameters = {};
+  try {
+    objParameters = validateRequiredQueryParameters(req, res, {
+      _id: {
+        type: 'StringArray',
+        isRequired: true,
+        str: '颜色id',
+      },
+      // 0: false, 1: true
+      action: {
+        type: 'Number',
+        isRequired: true,
+        str: '操作',
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err_code: 1,
+      message: `${err}`,
+    });
+  }
+  await dbUpdateManyById(req, res, ColorModel, objParameters, {
+    $set: { isDeleted: objParameters.action !== 1 },
+  });
+});
 
-module.exports = router
+module.exports = router;
